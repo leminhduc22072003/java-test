e0ff2226528c8a322fcd05a6479c4dd5635b6efe
duclm <leminhduc22072000@gmail.com>
2022-10-21 23:29:42 +0700

    update code

---
+++ src/main/java/com/example/test00/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.test00;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Xin chào tất cả các bạn!");
+    }
+}
